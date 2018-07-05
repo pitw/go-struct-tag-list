a9361d5fbc347f51227cbf4225c8ebbdcb1c561d
pitw <5676857+pitw@users.noreply.github.com>
2018-07-05 15:47:35 +0200

    Inital

---
+++ list/list.go
@@ -0,0 +1,55 @@
+package list
+
+import (
+	"reflect"
+	"strings"
+	"fmt"
+)
+
+func StructTagList(st interface{}, t string, ch bool) (string, error) {
+	// Loop through Struct st collect Tags t
+	c, err := loopStructTags(st, t, ch)
+
+	return stringsList(c), err
+}
+
+func loopStructTags(st interface{}, t string, ch bool) ([]string, error) {
+	v := reflect.ValueOf(st)
+	values := make([]interface{}, v.NumField())
+
+	var query []string
+
+	for i := 0; i < v.NumField(); i++ {
+		values[i] = v.Field(i).Interface()
+		field := reflect.TypeOf(st).FieldByIndex([]int{i})
+
+		k := getStructTag(field, t)
+		if (k != "") {
+			query = append(query, k)
+		}
+	}
+
+	err := checkStructComplete(v.NumField(), len(query), ch)
+
+	return query, err
+}
+
+// Check if struct contains same amount of tags like resulting list
+func checkStructComplete(s int, q int, ch bool) (error) {
+	// Just check if param set to True
+	if (s != q && ch == true) {
+		return fmt.Errorf("tags: Check Struct. QueryList() expects %v Tags. Got just %v", s, q)
+	} else {
+		return nil
+	}
+}
+
+// Return the specific tag of the struct
+func getStructTag(f reflect.StructField, t string) string {
+	return string(f.Tag.Get(t))
+}
+
+// Return comma separated list from []string
+func stringsList(s []string) (r string) {
+	return strings.Join(s, ",")
+}
